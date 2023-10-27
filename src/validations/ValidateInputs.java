d09b605146a866735e86aa00f5a3129998fb49d9
Vaishnav Puram <puramv307@gmail.com>
2023-10-27 15:23:02 +0530

    refactored

--- src/validations/ValidateInputs.java
+++ src/validations/ValidateInputs.java
@@ -1,6 +1,7 @@
 package validations;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import Exceptions.*;
@@ -8,7 +9,7 @@ import model.Customer;
 import model.ServicePlan.enumServicePlan;
 
 public class ValidateInputs {
-	private static void validatePlan(String p) throws InvalidPlanException {
+	public static String validatePlan(String p) throws InvalidPlanException {
 		boolean flag=false;
 		for(enumServicePlan e:enumServicePlan.values()) {
 			if(e.toString().equals(p)) {
@@ -19,26 +20,35 @@ public class ValidateInputs {
 		if(!flag) {
 			throw new InvalidPlanException("Invalid plan selected !");
 		}
+		return p;
 	}
-	private static void validateRegAmt(enumServicePlan plan,double amt) throws InvalidRegtAmt {
+	public static double validateRegAmt(enumServicePlan plan,double amt) throws InvalidRegtAmt {
 		
 		if(plan.getPlan()!=amt) {
 			throw new InvalidRegtAmt("Amount doesn't match with the plan selected !");
 		}
+		return amt;
 	}
-	private static void validateEmail(String email) throws InvalidEmail {
+	public static String validateEmail(String email) throws InvalidEmail {
 		if(!(Pattern.matches("^[a-z].*@gmail.com$", email))) {
 			throw new InvalidEmail("Invalid Email!");
+		}
+		return email;
+	}
+	public static void checkDupEmail(String email,List<Customer> customers) throws DuplicateEmailException {
+		if(customers.contains(email)) { //iterates till the size of the list
+			throw new DuplicateEmailException("Duplicate email!");
 		}
 	}
 	
 	public static Customer validateAll(String firstName, String lastName, String email, String password, double regAmt,
-			LocalDate dob, String plan) throws InvalidPlanException, InvalidRegtAmt, InvalidEmail {
-		validatePlan(plan);
-		validateRegAmt(enumServicePlan.valueOf(plan), regAmt);
-		validateEmail(email);
-		Customer c=new Customer( firstName,  lastName,  email,  password,  regAmt,
-				 dob,  enumServicePlan.valueOf(plan));
+			LocalDate dob, String plan,List<Customer> customers) throws InvalidPlanException, InvalidRegtAmt, InvalidEmail, DuplicateEmailException {
+		checkDupEmail(email, customers);
+		String validPlan=validatePlan(plan);
+		double validAmt=validateRegAmt(enumServicePlan.valueOf(plan), regAmt);
+		String validEmail=validateEmail(email);
+		Customer c=new Customer( firstName,  lastName,  validEmail,  password,  validAmt,
+				 dob,  enumServicePlan.valueOf(validPlan));
 		return c;
 	}
 }
