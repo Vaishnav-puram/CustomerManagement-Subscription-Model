0cff4cd5f5464af11d658024c5e4e534e04153dc
Vaishnav Puram <puramv307@gmail.com>
2023-10-27 16:30:16 +0530

    added Admin

---
+++ src/model/Admin.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Admin {
+	private static final String USERNAME="vaishnav";
+	private static final String PASSWORD="vaishv@123";
+	public static String getUsername() {
+		return USERNAME;
+	}
+	public static String getPassword() {
+		return PASSWORD;
+	}
+	
+}
